64a2ba5c889e042f45e12317ddd0207e773948ef
gonzalotev <gonzalotevez222@gmail.com>
2023-04-03 19:26:54 -0300

    feat: add about

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,71 @@
 import React from "react";
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const About = () => {
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+
   return (
-    <div className="section" id="about">
-      About
-    </div>
+    <section className="section" id="about" ref={ref}>
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
+          <motion.div
+            className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}></motion.div>
+          <motion.div
+            variants={fadeIn("left", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1">
+            <h2 className="h2 text-accent">About me.</h2>
+            <h3 className="h3 mb-4">Lorem Ipsum is simply dummy text</h3>
+            <p className="mb-6">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book
+            </p>
+            <div className="flex gap-x-6 lg:gap-x-10 mb-12">
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={2} duration={4} /> : null}
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Años de experiencia
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={2} duration={4} /> : null}
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Años de experiencia
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={2} duration={4} /> : null}
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Años de experiencia
+                </div>
+              </div>
+              <div>
+                <button>Contact me</button>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,84 @@
 import React from "react";
+import { BsArrowUpRight } from "react-icons/bs";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+
+const services = [
+  {
+    name: "Developer",
+    description:
+      "Developer asdasd a asds dasdasdasdasdasd a sdasdasdasdasd asd asdadasd",
+    link: "Learn more",
+  },
+  {
+    name: "Developer",
+    description:
+      "Developer asdasd a asds dasdasdasdasdasd a sdasdasdasdasd asd asdadasd",
+    link: "Learn more",
+  },
+  {
+    name: "Developer",
+    description:
+      "Developer asdasd a asds dasdasdasdasdasd a sdasdasdasdasd asd asdadasd",
+    link: "Learn more",
+  },
+];
 
 const Services = () => {
   return (
-    <div className="section" id="services">
-      Services
-    </div>
+    <section className="section" id="services">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0">
+            <h2 className="h2 text-accent mb-6">What i Do.</h2>
+            <h3 className="h3 max-2-[455px] mb-16 ">
+              tengo 2 años de experiencia como desarrollador de softaware
+            </h3>
+            <button className="btn btn-sm"> see my work</button>
+          </motion.div>
+          <motion.div
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1">
+            <div>
+              {services.map((service, index) => {
+                return (
+                  <div
+                    className="border-b border-white/20 h-[146px] mb-[38px] flex"
+                    key={index}>
+                    <div className="max-w-[476px]">
+                      <h4 className="text-[20px] tracking-wider font-primary font-semibold mb-6">
+                        {service.name}
+                      </h4>
+                      <p className="font-secondary leading-tight">
+                        {service.description}
+                      </p>
+                    </div>
+                    <div className="flex flex-col flex-1 items-end">
+                      <a
+                        href="#"
+                        className="btn w-9 h-9 mb-[42px] flex justify-center items-center">
+                        <BsArrowUpRight />
+                      </a>
+                      <a href="#" className="text-gradient text-sm">
+                        {service.link}
+                      </a>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
   );
 };
 
